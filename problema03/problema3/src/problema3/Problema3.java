1a46634d83d552bf55d5d4052b775a9aef7dbe43
fabian <frcanar2@utpl.edu.ec>
2023-11-16 23:03:25 -0500

    taller06

--- problema03/problema3/src/problema3/Problema3.java
+++ problema03/problema3/src/problema3/Problema3.java
@@ -18,9 +18,71 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         Scanner entrada = new Scanner(System.in);
+        Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+
+        String nombreempleado;
+        int tipoempleado;
+        double sueldo;
+        double incrementosueldo;
+        double sueldototal;
+
+        System.out.println("Nombre de empleado:");
+        nombreempleado = entrada.nextLine();
+        System.out.println("Ingrese el tipo de empleado"
+                + "Empleado tipo 1 dijite:1\n"
+                + "Empleado tipo 2 dijite:2\n"
+                + "Empleado tipo 3 dijite:3\n"
+                + "Empleado tipo 4 dijite:4\n"
+                + "Empleado tipo 5 dijite:5");
+        tipoempleado = entrada.nextInt();
+        System.out.println("Ingrese su sueldo");
+        sueldo = entrada.nextDouble();
+
+        incrementosueldo = 0;
+        sueldototal = sueldo;
+
+        switch (tipoempleado) {
+            case 1:
+
+                incrementosueldo = (sueldo * 5) / 100;
+                sueldototal = sueldo + incrementosueldo;
+                break;
+
+            case 2:
+
+                incrementosueldo = (sueldo * 7) / 100;
+                sueldototal = sueldo + incrementosueldo;
+                break;
+
+            case 3:
+
+                incrementosueldo = (sueldo * 9) / 100;
+                sueldototal = sueldo + incrementosueldo;
+
+                break;
+
+            case 4:
+
+                incrementosueldo = (sueldo * 9) / 100;
+                sueldototal = sueldo + incrementosueldo;
+
+                break;
+
+            case 5:
+
+                incrementosueldo = (sueldo * 15) / 100;
+                sueldototal = sueldo + incrementosueldo;
+
+                break;
+            default:
+                System.out.println("Opcion incorrecta");
+                break;
+        }
+
+        System.out.printf("Nombre de empleado: %s\n"
+                + "Incremento del sueldo: %.2f\n"
+                + "Suelo toltal:%.2f\n",
+                nombreempleado, incrementosueldo, sueldototal);
     }
-    
 }
